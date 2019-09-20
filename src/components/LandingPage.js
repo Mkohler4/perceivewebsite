c3953faa598cc2c41a02ac3a3c98d4884efc468d
Mkohler4 <Mkohler4@uwo.ca>
2019-09-20 01:12:16 -0400

    Ipad being set up more

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,9 +3,7 @@ import "../styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Perceive from "../images/Perceive.png";
 import Button from '@material-ui/core/Button';
-import About from "../images/About.png";
-import Estimate from "../images/Estimate.png";
-import Portfolio from "../images/Portfolio.png";
+import cellularIcon from '../images/cellularIcon.png'
 
 class LandingPage extends Component {
     constructor(props) {
@@ -38,7 +36,7 @@ class LandingPage extends Component {
                     <h3 style={{ flex: 12, fontSize: "20px" }}>Portfolio</h3>
                     <h3 style={{ flex: 12, fontSize: "20px" }}>Estimate</h3>
                     <h3 style={{ flex: 10, fontSize: "20px" }}>About</h3>
-                    <h3 style={{ flex: 12, fontSize: "20px", marginRight: "50px" }}>Contact</h3>
+                    <h3 style={{ flex: 12, fontSize: "20px", marginRight: "8%" }}>Contact</h3>
                 </div>
                 <div style={{ height: window.innerWidth < 900 ? "100%" : "80vh", display: "flex", flexDirection: "row" }}>
                     <div hidden={this.state.width < 900 ? true : false} className="col-5" style={{ flex: 1, justifyContent: "center", display: "flex", marginLeft: "8%", paddingLeft: "0" }}>
@@ -52,7 +50,10 @@ class LandingPage extends Component {
                     <div className="col-lg-7 col-md-12" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: this.state.width < 900 ? "center" : "baseline", marginRight: window.innerWidth < 900 ? "0" : "5%" }}>
                         <img src={Perceive} hidden={this.state.width < 900 ? true : false} className="perceiveLogo" style={{ height: "70%", position: "absolute", marginTop: "35px", left: "32%" }} />
                         <div className="iphone iphoneTransform">
-                            <div style={{ width: "100%", flex: 1 }}></div>
+                            <div hidden={this.state.width < 900 ? false : true} style={{ width: "100%", flex: 1, maxHeight: "50px", display: "flex", flexDirection: "row" }}>
+                                <img src={cellularIcon} style={{ transform: "scale(0.05)", justifySelf: "center", alignSelf: "center", flex: 1 }}></img>
+                                <h1 style={{ fontSize: "10px", flex: 1, placeSelf: "center" }}>Percieve</h1>
+                            </div>
                             <div style={{ width: "100%", flex: 12, display: "flex", flexDirection: "column" }}>
                                 <div style={{ width: "100%", flex: "1", display: "flex", flexDirection: "row" }}>
                                     <div className="appAboutImage" hidden={this.state.width < 900 ? false : true} style={{ height: "71px", maxWidth: "71px", borderRadius: "15px", flex: 1, margin: "auto" }} />
